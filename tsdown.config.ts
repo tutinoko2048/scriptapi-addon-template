2e56f22304d1ab969970d7cea5048ee5c59b727e
RetoRuto9900K <tutinoko2048@gmail.com>
2025-12-18 13:03:36 +0900

    init

---
+++ tsdown.config.ts
@@ -0,0 +1,8 @@
+import { defineConfig } from 'tsdown';
+
+export default defineConfig({
+  entry: 'src/main.ts',
+  outDir: 'scripts',
+  target: 'ES2024',
+  external: /^@minecraft\/(?!vanilla-data|math)/,
+});
